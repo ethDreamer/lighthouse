7ac15189d9dcf9da4e63c09595e044a2b3cc095a
Mark Mackey <mark@sigmaprime.io>
2022-08-08 16:05:45 -0500

    Refactored DepositTreeSnapshot

--- beacon_node/client/src/builder.rs
+++ beacon_node/client/src/builder.rs
@@ -382,7 +382,7 @@ where
                             info!(
                                 context.log(),
                                 "Loaded deposit tree snapshot";
-                                "deposits loaded" => deposit_snapshot.deposits,
+                                "deposits loaded" => deposit_snapshot.deposit_count,
                             );
                             Some(service)
                         }
